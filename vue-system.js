7ecc3fc492ab71bf9fa89c425dd8b76e69cca24f
zwh <751006992@qq.com>
2017-12-29 17:22:38 +0800

    d

--- vue-system.js
+++ vue-system.js
@@ -25,7 +25,7 @@ app.use(bodyParser.raw({
 app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'content-type,nonce,signature,timestamp,userid');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
